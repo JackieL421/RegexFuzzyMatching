b4c9c16905a4e237fd8c96dfa1366f48e583409b
Jackie Li <jackie.li@insigniafinancial.com.au>
2023-01-25 15:58:04 +1100

    Added user input for fuzzy searching

--- regex.py
+++ regex.py
@@ -3,8 +3,8 @@ from thefuzz import process
 
 choices = ["Atlanta Falcons", "New York Jets", "New York Giants", "Dallas Cowboys"]
 
-print(fuzz.ratio("this is a test", "this is a test!"))
-
-print(process.extract("new york jets", choices, limit=2))
+userInput = input('Enter the item you are looking for: ')
+regexLimit = int(input('Enter the fuzzy ratio: '))
+print(process.extract(userInput, choices, limit=regexLimit))
 
 # process.extractOne("cowboys", choices)
